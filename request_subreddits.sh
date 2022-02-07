ef755821da437d3dbbbe7aa2eda103e405390535
Joey Öhman <joeyoh@kth.se>
2022-02-07 18:49:39 +0100

    Add nested foor loop for downloads

--- request_subreddits.sh
+++ request_subreddits.sh
@@ -11,13 +11,15 @@
 
 
 # YEAR=2017
-SUBREDDIT="Halmstad"
+# SUBREDDIT="pinsamt"
 
 # declare -a years=(2021 2020 2019 2018 2017 2016 2015 2014 2013 2012 2011 2010 2009 2008 2007 2006)
-
+declare -a Subreddits=("PrivatEkonomi" "skellefte" "skitswedditsager" "Snus" "Spel" "spop" "stockholm" "Svenska" "svenskpolitik" "svepol" "sverigedemokraterna" "SWARJE" "swedents" "SwedishCopypasta" "swedishproblems" "swegan" "Swirclejerk" "umea" "uppsala" "Vasteras")
 # for year in "${years[@]}"; do
-for year in {2006..2017}; do
-   echo "Downloading subreddit $SUBREDDIT for year $year"
-   python3 pushshift_api.py --subreddit $SUBREDDIT --year $year
-   sleep 3
+for sub in "${Subreddits[@]}"; do
+  for year in {2006..2017}; do
+     echo "Downloading subreddit $sub for year $year"
+     python3 pushshift_api.py --subreddit $sub --year $year
+     sleep 3
+  done
 done
